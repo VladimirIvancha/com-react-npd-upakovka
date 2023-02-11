f9d1c69b3980c6cd119ff5f0b9a3396b9274df09
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2023-02-12 01:18:04 +0300

    feat: add carousel

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import TabPaneItem from "./TabPaneItem/TabPaneItem";
+
+import photo1 from "../../assets/images/alco_2-min.webp";
+import photo2 from "../../assets/images/Babaevskiy_packaging-min.webp";
+import photo3 from "../../assets/images/DesignDepot_2_calendar.webp";
 
 function About() {
   return (
@@ -23,7 +28,7 @@ function About() {
             </div>
           </div>
           <div className="container my-5 mx-auto text-center">
-            <div className="row">
+            <div className="row my-4">
               <div className="col">
                 <div className="border bg-primary fs-1 rounded">
                   <a
@@ -49,7 +54,7 @@ function About() {
                 </div>
               </div>
             </div>
-            <div className="row my-4">
+            <div className="row gy-4">
               <div className="col">
                 <div className="border bg-danger fs-1 rounded">
                   <a
@@ -89,13 +94,13 @@ function About() {
             </div>
           </div>
           <div className="row">
-            <div className="col-12 text-center  mt-5">
+            <div className="col-12 text-center mt-5">
               <div className="fs-4 fw-bold">Наши сертификаты</div>
             </div>
           </div>
           <div className="container my-5 mx-auto text-center">
             <div className="row">
-              <div className="col-12 col-md-4 text-center d-flex flex-column justify-content-between">
+              <div className="col-12 col-lg-4 text-center d-flex flex-column justify-content-between">
                 <div className="row">
                   <h3 className="fs-4">
                     Сертификат соответствия ISO 22000:2018
@@ -104,11 +109,15 @@ function About() {
                     <i className="fa-regular fa-file-pdf fs-1 icon__center"></i>
                   </div>
                 </div>
-                <a className="btn btn-success my-4 col-4 mx-auto" href="#" target="_blank">
+                <a
+                  className="btn btn-success my-4 col-4 mx-auto"
+                  href="#"
+                  target="_blank"
+                >
                   Скачать
                 </a>
               </div>
-              <div className="col-12 col-md-4 text-center d-flex flex-column justify-content-between">
+              <div className="col-12 col-lg-4 text-center d-flex flex-column justify-content-between">
                 <div className="row">
                   <h3 className="fs-4">
                     Декларация о соответствии требованиям технического
@@ -118,11 +127,15 @@ function About() {
                     <i className="fa-regular fa-file-pdf fs-1 icon__center"></i>
                   </div>
                 </div>
-                <a className="btn btn-success my-4 col-4 mx-auto" href="#" target="_blank">
+                <a
+                  className="btn btn-success my-4 col-4 mx-auto"
+                  href="#"
+                  target="_blank"
+                >
                   Скачать
                 </a>
               </div>
-              <div className="col-12 col-md-4 text-center d-flex flex-column justify-content-between">
+              <div className="col-12 col-lg-4 text-center d-flex flex-column justify-content-between">
                 <div className="row">
                   <h3 className="fs-4">
                     Сертификат соответствия ISO 22000:2005
@@ -131,9 +144,220 @@ function About() {
                     <i className="fa-regular fa-file-pdf fs-1 icon__center"></i>
                   </div>
                 </div>
-                <a className="btn btn-success my-4 col-4 mx-auto" href="#" target="_blank">
+                <a
+                  className="btn btn-success my-4 col-4 mx-auto"
+                  href="#"
+                  target="_blank"
+                >
                   Скачать
                 </a>
+              </div>
+            </div>
+          </div>
+          <div className="container about-tabs full-size d-none d-lg-block my-5">
+            <div className="row">
+              <div className="col-12 text-center mt-5">
+                <div className="fs-4 fw-bold">Наше оборудование</div>
+              </div>
+            </div>
+            <div className="row">
+              <ul className="nav justify-content-center my-5">
+                <li className="nav-item">
+                  <a
+                    className="nav-link active fs-5"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Все
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fs-5" href="#">
+                    Допечатное оборудование
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fs-5" href="#">
+                    Печатное оборудование
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fs-5" href="#">
+                    Послепечатное оборудование
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="row full-size">
+              {/* Tab panes */}
+              <div className="tab-content">
+                <div
+                  className="tab-pane active"
+                  role="tabpanel"
+                  id="tab-pane-all"
+                >
+                  <div className="row gy-4">
+                    {[1, 2, 3].map((item) => (
+                      <TabPaneItem key={item} />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container about-tabs mob-size d-lg-none my-5">
+            <div className="row">
+              <div className="col-12 text-center mt-5">
+                <div className="fs-4 fw-bold">Наше оборудование</div>
+              </div>
+            </div>
+            <div className="row">
+              <ul
+                className="nav justify-content-center my-5 d-block text-center"
+                role="tablist"
+              >
+                <li className="nav-item">
+                  <a
+                    className="nav-link active fs-5"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Все
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fs-5" href="#">
+                    Допечатное оборудование
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fs-5" href="#">
+                    Печатное оборудование
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fs-5" href="#">
+                    Послепечатное оборудование
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div
+              className="tab-pane active"
+              role="tabpanel"
+              id="tab-pane-all-mob"
+            >
+              <div
+                id="carouselExampleAutoplaying"
+                className="carousel slide"
+                data-bs-ride="carousel"
+              >
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <TabPaneItem />
+                  </div>
+                  <div className="carousel-item">
+                    <TabPaneItem />
+                  </div>
+                  <div className="carousel-item">
+                    <TabPaneItem />
+                  </div>
+                </div>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleAutoplaying"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleAutoplaying"
+                  data-bs-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="container about-portfolio my-5">
+            <div className="row">
+              <div className="col-12 text-center mt-5">
+                <div className="fs-2 fw-bold">Портфолио</div>
+              </div>
+            </div>
+            <div className="col-12 col-md-9 col-lg-6 mx-auto">
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide my-4"
+              >
+                <div className="carousel-indicators">
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="0"
+                    className="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                  ></button>
+                </div>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img src={photo1} className="d-block w-100" alt="Фото1" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={photo2} className="d-block w-100" alt="Фото2" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={photo3} className="d-block w-100" alt="Фото3" />
+                  </div>
+                </div>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
               </div>
             </div>
           </div>
---
+++ src/components/About/TabPaneItem/TabPaneItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function TabPaneItem() {
+  return (
+    <div className="col-sm-12 col-lg-6">
+      <div className="card text-center text-bg-info border">
+        <div className="card-body">
+          <h5 className="card-title mb-2">
+            Heidelberg Suprasetter 105 Gen II DCL
+          </h5>
+          <p className="card-text">
+            Термальная CtP-система для вывода печатных форм
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TabPaneItem;
