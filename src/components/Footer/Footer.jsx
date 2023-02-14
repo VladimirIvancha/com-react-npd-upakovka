ede11aee3c6404375cf3b63920ed524124afcbda
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2023-02-14 10:18:03 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,28 +10,28 @@ function Footer() {
           <div className="label text-center my-5">
             <img src={label} alt="Логотип" />
           </div>
-          <div class="container bottom_border">
-            <div class="row d-flex justify-content-between">
-              <div class="col-sm-4 col-12 col">
-                <h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
-                <p class="mb10">
+          <div className="container bottom_border">
+            <div className="row d-flex justify-content-between">
+              <div className="col-sm-4 col-12 col">
+                <h5 className="headin5_amrc col_white_amrc pt2">Find us</h5>
+                <p className="mb10">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
                   standard dummy text ever since the 1500s
                 </p>
                 <p>
-                  <i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35{" "}
+                  <i className="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35{" "}
                 </p>
                 <p>
-                  <i class="fa fa-phone"></i> +91-9999878398{" "}
+                  <i className="fa fa-phone"></i> +91-9999878398{" "}
                 </p>
                 <p>
-                  <i class="fa fa fa-envelope"></i> info@example.com{" "}
+                  <i className="fa fa fa-envelope"></i> info@example.com{" "}
                 </p>
               </div>
-              <div class=" col-sm-4 col-md col-6 col text-center">
-                <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
-                <ul class="footer_ul_amrc">
+              <div className=" col-sm-4 col-md col-6 col text-center">
+                <h5 className="headin5_amrc col_white_amrc pt2">Quick links</h5>
+                <ul className="footer_ul_amrc">
                   <li>
                     <a href="#">Image Rectoucing</a>
                   </li>
@@ -52,10 +52,10 @@ function Footer() {
                   </li>
                 </ul>
               </div>
-              <div class=" col-sm-4 col-md  col-6 col text-end">
-                <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+              <div className=" col-sm-4 col-md  col-6 col text-end">
+                <h5 className="headin5_amrc col_white_amrc pt2">Quick links</h5>
 
-                <ul class="footer_ul_amrc">
+                <ul className="footer_ul_amrc">
                   <li>
                     <a href="#">Remove Background</a>
                   </li>
@@ -76,13 +76,13 @@ function Footer() {
                   </li>
                 </ul>
               </div>
-              {/* <div class=" col-sm-4 col-md  col-12 col">
-                <h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+              {/* <div className=" col-sm-4 col-md  col-12 col">
+                <h5 className="headin5_amrc col_white_amrc pt2">Follow us</h5>
 
-                <ul class="footer_ul2_amrc">
+                <ul className="footer_ul2_amrc">
                   <li>
                     <a href="#">
-                      <i class="fab fa-twitter fleft padding-right"></i>{" "}
+                      <i className="fab fa-twitter fleft padding-right"></i>{" "}
                     </a>
                     <p>
                       Lorem Ipsum is simply dummy text of the printing...
@@ -91,7 +91,7 @@ function Footer() {
                   </li>
                   <li>
                     <a href="#">
-                      <i class="fab fa-twitter fleft padding-right"></i>{" "}
+                      <i className="fab fa-twitter fleft padding-right"></i>{" "}
                     </a>
                     <p>
                       Lorem Ipsum is simply dummy text of the printing...
@@ -100,7 +100,7 @@ function Footer() {
                   </li>
                   <li>
                     <a href="#">
-                      <i class="fab fa-twitter fleft padding-right"></i>{" "}
+                      <i className="fab fa-twitter fleft padding-right"></i>{" "}
                     </a>
                     <p>
                       Lorem Ipsum is simply dummy text of the printing...
@@ -112,8 +112,8 @@ function Footer() {
             </div>
           </div>
 
-          <div class="container">
-            <ul class="footer_bottom_ul_amrc">
+          <div className="container">
+            <ul className="footer_bottom_ul_amrc">
               <li>
                 <a href="#">Home</a>
               </li>
@@ -133,28 +133,28 @@ function Footer() {
                 <a href="#">Contact</a>
               </li>
             </ul>
-            <p class="text-center">
+            <p className="text-center">
               &#169; ООО «NPD-упаковка», 2019–2023 Все права защищены
             </p>
-            <ul class="social_footer_ul">
+            <ul className="social_footer_ul">
               <li>
                 <a href="#">
-                  <i class="fab fa-facebook-f"></i>
+                  <i className="fab fa-facebook-f"></i>
                 </a>
               </li>
               <li>
                 <a href="#">
-                  <i class="fab fa-twitter"></i>
+                  <i className="fab fa-twitter"></i>
                 </a>
               </li>
               <li>
                 <a href="#">
-                  <i class="fab fa-linkedin"></i>
+                  <i className="fab fa-linkedin"></i>
                 </a>
               </li>
               <li>
                 <a href="#">
-                  <i class="fab fa-instagram"></i>
+                  <i className="fab fa-instagram"></i>
                 </a>
               </li>
             </ul>
